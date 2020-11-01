24302593f8ed35397ac6a6e64d336c5f4d03d805
your name <1092130900@qq.com>
2020-11-01 18:35:45 +0800

    更新

--- mms/src/App.jsx
+++ mms/src/App.jsx
@@ -1,21 +1,45 @@
-import React, { Suspense, lazy } from 'react';
-import { withRouter, useHistory, useLocation, useRouteMatch, useParams, Route, Switch, Redirect } from 'react-router-dom';
+import React, { Suspense, lazy, useCallback, useEffect, useState, useLayoutEffect} from 'react';
+import { withRouter, useHistory,useLocation, useRouteMatch, useParams, Route, Switch, Redirect } from 'react-router-dom';
 import logo from '@/logo.svg';
+
 import './App.scss';
 import "./common/reset.css"
 
 
 const Home = lazy(() => import("./views/Home"));
-const Mine = lazy(() => import("./views/Mine"));
+// const User = lazy(() => import("./views/User"));
+// const Order = lazy(() => import("./views/Order"));
+// const Collect = lazy(() => import("./views/Collect"));
+// const Goods = lazy(() => import("./views/Goods"));
+const Reg = lazy(() => import("./views/Reg"));
 const Login = lazy(() => import("./views/Login"));
 
 function App(props) {
+
   const menu = [
     {
       text: '首页',
       path: '/home',
       name: 'home',
       component: Home,
+      // route:[
+      //   {
+      //     path:"/home/user",
+      //     component:User
+      //   },
+      //   {
+      //     path:"/home/goods",
+      //     component:Goods
+      //   },
+      //   {
+      //     path:"/home/order",
+      //     component:Order
+      //   },
+      //   {
+      //     path:"/home/collect",
+      //     component:Collect
+      //   },
+      // ]
     },
     {
       text: '登录',
@@ -29,17 +53,26 @@ function App(props) {
       component: Reg
     }
   ]
-  return (
-    <div className="App">
-      <Suspense fallback={<div>loading...</div>}></Suspense>
-      <Switch>
-        <Route path='/home' component={Home} />
-        <Route path='/login' component={Login} />
-        <Route path='/reg' component={Reg} />
-      </Switch>
-      <Suspense />
-    </div>
-  );
-}
 
-export default App;
+  return (
+      <div>
+            <Suspense fallback={<div>loading...</div>}>
+            <Switch>
+              <Route path='/home' component={Home} />
+              <Route path='/login' component={Login} />
+              <Route path='/reg' component={Reg} />
+              {/* <Route path='/home/user' component={User} />
+              <Route path='/home/goods' component={Goods} />
+              <Route path='/home/order' component={Order} />
+              <Route path='/home/collect' component={Collect} /> */}
+              <Route path="/notfound" render={() => <div>404</div>}></Route>
+              {/* 重定向 有3个属性from to  exact*/}
+              <Redirect from='/' to='/home' exact />
+              {/* 如果匹配不成功就跳到这里 */}
+              <Redirect to="/notfound" />
+            </Switch>
+            </Suspense>
+      </div>
+  )
+}
+ export default App;
---
+++ mms/src/views/Goods/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.scss'
+
+const Goods = () => {
+    return (
+        <div>
+          菜品管理
+        </div>
+    )
+}
+export default Goods
