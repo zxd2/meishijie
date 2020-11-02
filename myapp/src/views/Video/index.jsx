200739d18e0c41f266130819c4c39c96805892ad
谢培焕 <l>
2020-11-02 11:45:10 +0800

    修改了一点视频

--- myapp/src/views/Video/index.jsx
+++ myapp/src/views/Video/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import request from '@/utils/request';
-const Video = () => {
+import { NavBar, Icon } from 'antd-mobile';
+const Video = () => { 
+
     return (
-        <div>登录</div>
+        <div>
+            <NavBar
+              mode="light"
+              icon={<Icon type="left" />}
+              onLeftClick={()=>{props.history.push('/home')}}
+              rightContent={[
+                <Icon key="0" type="search" style={{ marginRight: '16px' }} onClick={()=>{props.history.push('/search')}} />
+              ]}
+            >菜谱视频大全</NavBar>
+        </div>
     )
 }
 export default Video
